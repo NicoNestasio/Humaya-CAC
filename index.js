8e25a0b1eea5d3522f6177c74f5cf3e1dd3e46cb
Nico Nestasio <niconesta66@gmail.com>
2023-09-29 14:42:33 -0300

    A la versión del 29/9/23, las cards de recetas se deben mejorar para que funciones de forma más responsive, se podría encontrar una solución a las imagenesque no están disponibles en la api, y se superponen los títulos del footer.

--- index.js
+++ index.js
@@ -2,22 +2,54 @@
 fetch('https://sazonapi.hymsoft.repl.co/api/v1/recipies')
       .then(response => response.json())
       .then(postre => mostrarData(postre))
-const min = 0; // Valor mínimo (inclusive)
-const max = 100; // Valor máximo (inclusive)
-const i = Math.floor(Math.random() * (max - min + 1)) + min;
-const mostrarData = (postre) =>{
+let i, j, k;
+
+const mostrarData = (postre) => {
+
+      const num = [];
+      while (num.length < 3) {
+            const num1 = Math.floor(Math.random() * (postre.data.length));
+            if (!num.includes(num1)) {
+                  num.push(num1);
+            }
+      }
+
+      [i, j, k] = num;
+      console.log("i:", i);
+      console.log("j:", j);
+      console.log("k:", k);
       console.log(postre)
-      document.getElementById('imgreceta1').src= postre.data[0].imagen;
-      document.getElementById('hreceta1').innerHTML=postre.data[0].nombre;
-      document.getElementById('imgreceta2').src= postre.data[1].imagen;
-      document.getElementById('hreceta2').innerHTML=postre.data[1].nombre;
-      document.getElementById('imgreceta3').src= postre.data[2].imagen;
-      document.getElementById('hreceta3').innerHTML=postre.data[2].nombre;
+      //Receta1
+      document.getElementById('imgreceta1').src = postre.data[i].imagen;
+      document.getElementById('hreceta1').innerHTML = postre.data[i].nombre;
+      document.getElementById('preceta1').innerHTML = postre.data[i].descripcion_tipo;
+      for (var f = 0; f < postre.data[i].ingredientes.length; f++) {
+            var ingrediente = postre.data[i].ingredientes[f];
+            var listItem = document.createElement('li'); // Crear un nuevo elemento li
+            listItem.textContent = ingrediente.cantidad + ' de ' + ingrediente.nombre; // Establecer el contenido del li
+            document.getElementById('ingredientesr1').appendChild(listItem);
+        }
+        document.getElementById('instruccionesr1').innerHTML = postre.data[k].instrucciones;
+      //Receta2
+      document.getElementById('imgreceta2').src = postre.data[j].imagen;
+      document.getElementById('hreceta2').innerHTML = postre.data[j].nombre;
+      document.getElementById('preceta2').innerHTML = postre.data[j].descripcion_tipo;
+      for (var h = 0; h < postre.data[j].ingredientes.length; h++) {
+            var ingrediente = postre.data[j].ingredientes[h];
+            var listItem = document.createElement('li'); // Crear un nuevo elemento li
+            listItem.textContent = ingrediente.cantidad + ' de ' + ingrediente.nombre; // Establecer el contenido del li
+            document.getElementById('ingredientesr2').appendChild(listItem);
+        }
+        document.getElementById('instruccionesr2').innerHTML = postre.data[j].instrucciones;
+      //Receta3
+      document.getElementById('imgreceta3').src = postre.data[k].imagen;
+      document.getElementById('hreceta3').innerHTML = postre.data[k].nombre;
+      document.getElementById('preceta3').innerHTML = postre.data[k].descripcion_tipo;
+      for (var r = 0; r < postre.data[k].ingredientes.length; r++) {
+            var ingrediente = postre.data[k].ingredientes[r];
+            var listItem = document.createElement('li'); // Crear un nuevo elemento li
+            listItem.textContent = ingrediente.cantidad + ' de ' + ingrediente.nombre; // Establecer el contenido del li
+            document.getElementById('ingredientesr3').appendChild(listItem);
+        }
+        document.getElementById('instruccionesr3').innerHTML = postre.data[k].instrucciones;
 }
-   //   var genero = gen(persona.results[0].gender);
-    //   document.getElementById('nombre').innerHTML= persona.results[0].name.first;
-    //   document.getElementById('apellido').innerHTML= persona.results[0].name.last;
-    //   document.getElementById('imagen').src= persona.results[0].picture.large;
-    //   document.getElementById('email').innerHTML= persona.results[0].email;
-    //   document.getElementById('cel').innerHTML= persona.results[0].cell;
-    //   document.getElementById('infop').innerHTML= "<li>Edad: " + persona.results[0].dob.age + " años</li>" + "<li>Género: " + genero + "</li>" + "<li>Nacionalidad: " + persona.results[0].nat + "</li>" + "<li>Residencia: " + persona.results[0].location.city + ", " + persona.results[0].location.state + ", " + persona.results[0].location.country + "</li>" +  "<li>ID: " + persona.results[0].id.name + " " + persona.results[0].id.value + "</li>"
